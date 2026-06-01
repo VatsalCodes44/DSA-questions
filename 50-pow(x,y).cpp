f67dd61ab8ea6a246e113a005309e887298b17a1
Vatsal Mahajan <mahajanvatsal44@gmail.com>
2026-06-01 18:38:19 +0530

    pow optimal solution

--- 50-pow(x,y).cpp
+++ 50-pow(x,y).cpp
@@ -22,6 +22,25 @@ double myPowBruteForce(double x, int n) {
     return ans;
 }
 
-int main () {
-    cout << myPowBruteForce(6, 2);
+
+// optimal solution
+double pow (double x, long n) {
+    if (n==0) return 1;
+    if (n%2 == 0) {
+        return pow(x*x, n/2);
+    }
+    else return x*pow(x, n-1);
+}
+double myPow(double x, int n) {
+    double ans = 1;
+
+
+    bool negative = n < 0 ? true : false;
+
+    long nm = n < 0 ? (long)(n)*-1 : n;
+
+    ans = pow (x, nm);
+
+    if (negative) return 1/ans;
+    return ans;
 }
