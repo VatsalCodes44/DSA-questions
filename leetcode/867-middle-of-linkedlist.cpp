63334f2330137eac8a39217dfc65904e63d3165d
Vatsal Mahajan <mahajanvatsal44@gmail.com>
2026-06-02 20:27:07 +0530

    middle of LL

---
+++ leetcode/867-middle-of-linkedlist.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+ListNode* middleNode(ListNode* head) {
+    if (!head || !head->next) return head;
+    ListNode* slow = head;
+    ListNode* fast = head;
+    while (fast && fast->next) {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    return slow;
+}
+
+int main () {
+    ListNode* head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+
+    cout << middleNode(head)->val;
+}
