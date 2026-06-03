1bc1a7b607feb3bc579555ce8ce86ca399a5ef21
Vatsal Mahajan <mahajanvatsal44@gmail.com>
2026-06-03 17:17:38 +0530

    sort stack using rcursion

---
+++ leetcode/LL/206-reverseLL.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+ListNode* reverseList(ListNode* head) {
+    if (!head || !head->next) return head;
+
+    ListNode* curr = head;;
+    ListNode* prev = nullptr;
+
+    while (curr) {
+        ListNode* temp = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = temp;
+    }
+    return prev;
+}
+
+int main () {}
---
+++ tuf/recursion/sort-stack.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void insertInOrder(stack<int> &st, int x) {
+    if (st.empty()) {
+        st.push(x);
+        return;
+    }
+
+    if (st.top() > x) {
+        int top = st.top();
+        st.pop();
+        insertInOrder(st, x);
+        st.push(top);
+    }
+    else {
+        st.push(x);
+    }
+}
+
+void sortStack(stack<int>& st) {
+    if (st.size() == 0 ) return;
+    int x = st.top();
+    st.pop();
+    sortStack(st);
+
+    insertInOrder(st, x);
+}
+
+void printStack(stack<int> st) {
+     while (!st.empty()){
+        cout << st.top() << " ";
+        st.pop();
+    }
+}
+
+int main () {
+    stack<int> st;
+    st.push(9);
+    st.push(7);
+    st.push(3);
+    st.push(1);
+    st.push(8);
+    st.push(2);
+
+    sortStack(st);
+
+    cout << st.size() << endl;
+
+    printStack(st);
+
+}
