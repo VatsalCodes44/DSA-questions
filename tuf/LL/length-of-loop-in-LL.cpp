acd64fdcb841096f51b82757c81aa0d40bb361f0
Vatsal Mahajan <mahajanvatsal44@gmail.com>
2026-06-06 19:49:27 +0530

    Length of loop in LL

---
+++ tuf/LL/length-of-loop-in-LL.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode()
+    {
+        val = 0;
+        next = NULL;
+    }
+    ListNode(int data1)
+    {
+        val = data1;
+        next = NULL;
+    }
+    ListNode(int data1, ListNode *next1)
+    {
+        val = data1;
+        next = next1;
+    }
+};
+
+int findLengthOfLoop(ListNode *head) {
+    if (!head || !head->next) return 0;
+    ListNode* slow = head;
+    ListNode* fast = head;
+
+    while (fast && fast->next) {
+        slow = slow->next;
+        fast = fast->next->next;
+
+        if (slow == fast) {
+            int count = 0;
+            do {
+                count++;
+                fast = fast->next;
+            } while (slow != fast);
+        }
+    }
+}
