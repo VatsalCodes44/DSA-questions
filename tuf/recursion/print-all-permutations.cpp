8eb783e2605d370e699ac90ea411937b3f1df20b
Vatsal Mahajan <mahajanvatsal44@gmail.com>
2026-06-14 00:12:07 +0530

    optimised space complexity

--- tuf/recursion/print-all-permutations.cpp
+++ tuf/recursion/print-all-permutations.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-void permutations(vector<int> &arr, vector<int> &freq, string &ans) {
+void appproach1(vector<int> &arr, vector<int> &freq, string &ans) {
     if (ans.size() == arr.size()) {
         cout << ans << endl;
         return;
@@ -11,18 +11,39 @@ void permutations(vector<int> &arr, vector<int> &freq, string &ans) {
     for (int j = 0; j < freq.size(); j++) {
         if (freq[j]) {
             freq[j]--;
-            ans.push_back(arr[j]);
-            permutations(arr, freq, ans);
+            ans.push_back('0'+arr[j]);
+            appproach1(arr, freq, ans);
             ans.pop_back();
             freq[j]++;
         }
     }
 }
 
+void approach2 (vector<int> arr, int i) {
+    if (i == arr.size()) {
+        for (auto x: arr) {
+            cout << x << " ";
+        }
+        cout << endl;
+        return;
+    }
+
+    for (int j = i; j < arr.size(); j++) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+        approach2(arr, i+1);
+        temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
+
 int main () {
-    vector <int> arr = {'1','2','3'};
+    vector <int> arr = {1,2,3};
     vector <int> freq = {1,1,1};
     string ans = "";
-    permutations(arr, freq, ans);
+    appproach1(arr, freq, ans);
+    // approach2(arr, 0);
     
 }
