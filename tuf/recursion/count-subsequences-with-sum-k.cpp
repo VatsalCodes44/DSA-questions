b06b9d63431e8467a875a915aff0716dd3c607c7
Vatsal Mahajan <mahajanvatsal44@gmail.com>
2026-06-15 02:39:41 +0530

    count sub seq with sum k

--- tuf/recursion/count-subsequences-with-sum-k.cpp
+++ tuf/recursion/count-subsequences-with-sum-k.cpp
@@ -20,6 +20,25 @@ int printSub(vector<int>& arr, int i, int sum, int k) {
     return left + right; // no need just to avoid compiler warning of non void function
 }
 
+// iterative approach
+int countSub2(vector<int>& arr, int i, int k, string& ans) {
+    if (k < 0) return 0;
+    if (k == 0) return 1;
+    if (i == arr.size()) {
+        return 0;
+    }
+
+    int count = 0;
+    for (int j = i; j < arr.size(); j++) {
+        k -= arr[j];
+        ans.push_back('0'+arr[j]);
+        count += countSub2(arr, j+1, k, ans);
+        ans.pop_back();
+        k += arr[j];
+    }
+    return count;
+}
+
 int main () {
     vector <int> arr = {1,2,3};
     cout << printSub(arr, 0, 0, 3) << endl;
