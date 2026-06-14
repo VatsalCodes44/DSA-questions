8f7155230b44f308b71484bd261cc5a94598f2fe
Vatsal Mahajan <mahajanvatsal44@gmail.com>
2026-06-15 02:29:14 +0530

    count sub seq with sum k

--- revision/count-subseq-with-sum-k.cpp
+++ revision/count-subseq-with-sum-k.cpp
@@ -17,7 +17,26 @@ int countSub(vector<int>& arr, int i, int k){
     return left + right;
 }
 
+int countSub2(vector<int>& arr, int i, int k, string& ans) {
+    if (k < 0) return 0;
+    if (k == 0) return 1;
+    if (i == arr.size()) {
+        return 0;
+    }
+
+    int count = 0;
+    for (int j = i; j < arr.size(); j++) {
+        k -= arr[j];
+        ans.push_back('0'+arr[j]);
+        count += countSub2(arr, j+1, k, ans);
+        ans.pop_back();
+        k += arr[j];
+    }
+    return count;
+}
+
 int main () {
     vector<int> arr = {4, 9, 2, 5, 1};
-    cout << countSub(arr, 0, 10) << endl;
+    string ans = "";
+    cout << countSub2(arr, 0, 10, ans) << endl;
 }
