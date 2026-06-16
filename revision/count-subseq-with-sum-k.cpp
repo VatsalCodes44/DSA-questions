0e62c01fd558f791fa5de0fc7311f7749c794527
Vatsal Mahajan <mahajanvatsal44@gmail.com>
2026-06-16 23:27:24 +0530

    count subseq

--- revision/count-subseq-with-sum-k.cpp
+++ revision/count-subseq-with-sum-k.cpp
@@ -2,20 +2,7 @@
 
 using namespace std;
 
-int countSub(vector<int>& arr, int i, int k){
-    if (k < 0) return 0;
-    if (i == arr.size()) {
-        if (k == 0) return 1;
-        return 0;
-    }
 
-    k -= arr[i];
-    int right = countSub(arr, i+1, k);
-    k+= arr[i];
-
-    int left = countSub(arr, i+1, k);
-    return left + right;
-}
 
 int countSub2(vector<int>& arr, int i, int k, string& ans) {
     if (k < 0) return 0;
