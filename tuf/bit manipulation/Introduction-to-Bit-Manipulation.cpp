8c7fae49db7ace292ef00d7147c0470af31720f7
Vatsal Mahajan <mahajanvatsal44@gmail.com>
2026-06-26 22:08:49 +0530

    binaryToDecimal

--- tuf/bit manipulation/Introduction-to-Bit-Manipulation.cpp
+++ tuf/bit manipulation/Introduction-to-Bit-Manipulation.cpp
@@ -4,14 +4,23 @@ using namespace std;
 
 string decimalToBinary(int n) {
     string str = "";
-    while (n) {
-        str.push_back(n%2 == 0 ? '0' : '1');
+    while (n) { // O(logn)
+        str.push_back(n%2 == 0 ? '0' : '1'); // O(1)
         n /= 2;
     }
-    reverse(str.begin(),str.end());
+    reverse(str.begin(),str.end()); // O(n/2)
     return str;
 }
 
+int binaryToDecimal(string binary) {
+    int n = 0;
+    for (int i = 0; i < binary.size(); i++) {
+        n += binary[i] == '1' ? (1 << binary.size()-i-1) : 0;
+    }
+    return n;
+}
+
 int main () {
-    cout << decimalToBinary(13);
+    string str = "1101";
+    cout << binaryToDecimal(str) <<endl;
 }
