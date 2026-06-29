5d4a5665ba6c089d041f995feaaf6d077750b819
Vatsal Mahajan <mahajanvatsal44@gmail.com>
2026-06-29 16:57:06 +0530

    Generate Parentheses

--- revision/generate-parenthesis.cpp
+++ revision/generate-parenthesis.cpp
@@ -2,29 +2,26 @@
 
 using namespace std;
 
-void f(vector<string> &ans, string& str, int open, int close, int i, int n) {
-    if (i == n*2) {
-        ans.push_back(str);
+void f (int i, int n, int open, int close, string& ans){
+    if (i/2 == n) {
+        cout << ans << endl;
         return;
     }
+
     if (open < n) {
-        str.push_back('(');
-        open++;
-        f(ans, str, open, close, i+1, n);
-        open--, 
-        str.pop_back();
+        ans.push_back('(');
+        f (i+1, n, open+1, close, ans);
+        ans.pop_back();
     }
+
     if (close < open) {
-        str.push_back(')');
-        close++;
-        f(ans, str, open, close, i+1, n);
-        close--, 
-        str.pop_back(); 
+        ans.push_back(')');
+        f(i+1, n, open, close+1, ans);
+        ans.pop_back();
     }
 }
-vector<string> generateParenthesis(int n) {
-    vector<string> ans;
-    string str;
-    f(ans, str, 0, 0, 0, n);
-    return ans;
+
+int main () {
+    string ans = "";
+    f(0, 3, 0, 0, ans);
 }
