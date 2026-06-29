0aa535c5355eb05e0f95a7be53632e3bfafc1f50
Vatsal Mahajan <mahajanvatsal44@gmail.com>
2026-06-29 19:50:32 +0530

    print-subseq-with-sum-k

--- revision/print-subseq-with-sum-k.cpp
+++ revision/print-subseq-with-sum-k.cpp
@@ -2,28 +2,51 @@
 
 using namespace std;
 
-void f (vector<int>& arr, int i, int target, vector<int>& ans) {
+void approach1 (vector<int>& arr, int i, int target, vector<int>& ans) {
+    if (target < 0) return;
+    if (i == arr.size()) {
+        if (target == 0) {
+            for (auto x: ans) {
+                cout << x << " "; 
+            }
+            cout << endl;
+            return;
+        }
+        return;
+    }
+
+    ans.push_back(arr[i]);
+    target -= arr[i];
+    approach1(arr, i+1, target, ans);
+    ans.pop_back();
+    target += arr[i];
+
+    approach1(arr, i+1, target, ans);
+}
+
+void approach2 (vector<int>& arr, int i, int target, vector<int>& ans) {
     if (target < 0) return;
     if (target == 0) {
         for (auto x: ans) {
-            cout << x << " "; 
+            cout << x << " ";
         }
         cout << endl;
         return;
     }
+
     if (i == arr.size()) return;
 
-    ans.push_back(arr[i]);
-    target -= arr[i];
-    f(arr, i+1, target, ans);
-    ans.pop_back();
-    target += arr[i];
-
-    f(arr, i+1, target, ans);
+    for (int j = i; j < arr.size(); j++) {
+        ans.push_back(arr[j]);
+        target -= arr[j];
+        approach2(arr, j+1, target, ans);
+        ans.pop_back();
+        target += arr[j];
+    }
 }
 
 int main () {
     vector<int> arr = {1,2,3};
     vector<int> ans;
-    f(arr, 0, 3, ans);
+    approach2(arr, 0, 3, ans);
 }
