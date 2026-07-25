9f9a304b0cc3c83666888e16b5375da504fea759
Vatsal Mahajan <mahajanvatsal44@gmail.com>
2026-07-26 04:21:26 +0530

    155-Min-Stack

---
+++ leetcode/stack-data-structure/155-Min-Stack.cpp
@@ -0,0 +1,115 @@
+// Methods pop, top and getMin operations will always be called on non-empty stacks.#include <bits/stdc++.h>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// using linked list
+// class MinStack {
+//     struct ListNode {
+//         int val;
+//         int mini;
+//         ListNode* next;
+
+//         ListNode(int x, int mini) {
+//             val = x;
+//             this->mini = mini;
+//             next = nullptr;
+//         }
+//         ListNode(int x, int mini, ListNode* next) {
+//             this->val = x;
+//             this->mini = mini;
+//             this->next = next;
+//         }
+//     };
+//     ListNode* head;
+//     int currSize;
+
+// public:
+//     MinStack() {
+//         head = nullptr;
+//         currSize = 0;
+//     }
+
+//     ~MinStack() {
+//         while (head) {
+//             ListNode* temp = head;
+//             head = head->next;
+//             delete temp;
+//         }
+//     }
+    
+//     void push(int value) {
+//         if (!head) {
+//             head = new ListNode(value, value);
+//             currSize = 1;
+//             return;
+//         }
+
+//         ListNode* temp = new ListNode(value, head->mini < value ? head->mini : value, head);
+//         head = temp;
+//         currSize++;
+//     }
+    
+//     void pop() {
+//         if (!head->next) {
+//             delete head;
+//             currSize = 0;
+//             head = nullptr;
+//             return;
+//         }
+//         ListNode* temp = head;
+//         head = head->next;
+//         delete temp;
+//         currSize--;
+//     }
+    
+//     int top() {
+//         return head->val;
+//     }
+    
+//     int getMin() {
+//         return head->mini;
+//     }
+// };
+
+
+// // using vector and pair<int, int> data structure
+// class MinStack {
+//     vector <pair<int, int>> arr;
+//     int currSize;
+
+// public:
+//     MinStack() {
+//         currSize = 0;
+//     }
+    
+//     void push(int value) {
+//         if (arr.size() == 0) {
+//             arr.push_back({value, value});
+//             currSize = 1;
+//             return;
+//         }
+
+//         arr.push_back({value, (arr.back().second) < value ? arr.back().second : value});
+//         currSize++;
+//     }
+    
+//     void pop() {
+//         arr.pop_back();
+//         currSize--;
+//     }
+    
+//     int top() {
+//         return arr.back().first;
+//     }
+    
+//     int getMin() {
+//         return arr.back().second;
+//     }
+// };
+
+
+
+int main () {
+    vector <int> arr;
+}
