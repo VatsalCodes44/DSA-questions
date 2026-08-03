894b61919ad5e65bcd0330037dd4344b80a5b42f
Vatsal Mahajan <mahajanvatsal44@gmail.com>
2026-08-04 00:33:56 +0530

    nextGreaterElement optimal

--- leetcode/stack-data-structure/496-Next-Greater-Element-I.cpp
+++ leetcode/stack-data-structure/496-Next-Greater-Element-I.cpp
@@ -3,24 +3,23 @@
 using namespace std;
 
 vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-    vector<int> ans;
-    unordered_map<int, int> mpp;
-    for (int i = 0; i < nums2.size(); i++) {
-        mpp[nums2[i]] = i;
+    map<int, int> mpp;
+    stack<int> st;
+    
+    for (int i = nums2.size()-1; i >= 0; i--) {
+        while (!st.empty() && st.top() < nums2[i]) {
+            st.pop();
+        }
+        if (st.empty()) {
+            mpp[nums2[i]] = -1;
+        }
+        else mpp[nums2[i]] = st.top();
+        st.push(nums2[i]);
     }
+
+    vector<int>ans(nums1.size(), -1);
     for (int i = 0; i < nums1.size(); i++) {
-        int j = mpp[nums1[i]]+1;
-        bool pushed = false;
-        for (j; j < nums2.size(); j++) {
-            if (nums1[i] < nums2[j]) {
-                pushed = true;
-                ans.push_back(nums2[j]);
-                break;
-            }
-        }
-        if (!pushed) {
-            ans.push_back(-1);
-        }
+        ans[i] = mpp[nums1[i]];
     }
     return ans;
 }
