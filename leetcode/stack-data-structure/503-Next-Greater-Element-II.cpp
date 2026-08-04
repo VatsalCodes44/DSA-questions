544617c6d67f1d642d97bcb29989b788dcf60561
Vatsal Mahajan <mahajanvatsal44@gmail.com>
2026-08-05 00:14:00 +0530

    nextGreaterElements

---
+++ leetcode/stack-data-structure/503-Next-Greater-Element-II.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<int> nextGreaterElements(vector<int>& arr) {
+    stack<int> st;
+    vector<int> ans(arr.size(), -1);
+
+    for (int i = arr.size()-1; i >= 0; i--) {
+        while (!st.empty() && st.top() <= arr[i]) {
+            st.pop();
+        }
+
+        st.push(arr[i]);
+    }
+
+    for (int i = arr.size()-1; i >= 0; i--) {
+        while (!st.empty() && st.top() <= arr[i]) {
+            st.pop();
+        }
+
+        if (!st.empty()) {
+            ans[i] = st.top();
+        }
+        st.push(arr[i]);
+    }
+    return ans;
+}
