5547bb973ce96dedfc79ce9d8de30487081675fd
Vatsal Mahajan <mahajanvatsal44@gmail.com>
2026-08-10 00:33:35 +0530

    max prod sub array

--- leetcode/arrays/152-Maximum-Product-Subarray.cpp
+++ leetcode/arrays/152-Maximum-Product-Subarray.cpp
@@ -3,50 +3,16 @@
 using namespace std;
 
 int maxProduct(vector<int>& nums) {
+    int result = nums[0];
     int maxProd = nums[0];
-    int positiveProd = nums[0] > 0 ? nums[0] : 0;
-    int negativeProd = nums[0] < 0 ? nums[0] : 0;
+    int minProd = nums[0];
     for (int i = 1; i < nums.size(); i++) {
-        if (positiveProd == 0) {
-            positiveProd = max(0, nums[i]);
-        }
-        else {
-            positiveProd *= nums[i];
-        }
-        if (negativeProd == 0) {
-            negativeProd = min(0, nums[i]);
-        }
-        else {
-            negativeProd *= nums[i];
-        }
-
-        if (positiveProd >= 0) {
-            maxProd = max(maxProd, positiveProd);
-        }
-        if (negativeProd >= 0) {
-            maxProd = max(maxProd, negativeProd);
-        }
-
-        if (negativeProd > 0 && positiveProd < 0) {
-            int temp1 = negativeProd;
-            int temp2 = positiveProd;
-
-            negativeProd = min(temp1, temp2);
-            positiveProd = max(temp1, temp2);
-        }
-
-        if (negativeProd > 0) {
-            positiveProd = max(positiveProd, negativeProd);
-            negativeProd = min(0, nums[i]);
-        }
-        if (positiveProd < 0) {
-            negativeProd = min(positiveProd, negativeProd);
-            positiveProd = max(0, nums[i]);
-        }
-
+        if (nums[i] < 0) swap(maxProd, minProd);
+        maxProd = max(nums[i], maxProd*nums[i]);
+        minProd = min(nums[i], minProd*nums[i]);
+        result = max(result, maxProd);
     }
-
-    return maxProd;
+    return result;
 }
 
 int main () {
