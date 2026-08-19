57f37f054d3dcbc4a7ff9529fc8b6fe2685d7cc4
Vatsal Mahajan <mahajanvatsal44@gmail.com>
2026-08-19 23:25:34 +0530

    subArrayRanges

--- leetcode/stack-data-structure/2104-SumofSubarrayRanges.cpp
+++ leetcode/stack-data-structure/2104-SumofSubarrayRanges.cpp
@@ -6,12 +6,12 @@ class Solution {
 public:
     long long subArrayRanges(vector<int>& arr) {
         stack<int> st;
-        vector<int> neLeft(arr.size(), -1);
-        vector<int> neRight(arr.size(), -1);
+        vector<int> nseLeft(arr.size(), -1);
+        vector<int> nseRight(arr.size(), -1);
 
         for (int i = 0; i < arr.size(); i++) {
             while (!st.empty() && arr[st.top()] >= arr[i]) st.pop();
-            if (!st.empty()) neLeft[i] = st.top();
+            if (!st.empty()) nseLeft[i] = st.top();
             st.push(i);
         }
 
@@ -19,28 +19,30 @@ public:
 
         for (int i = arr.size() - 1; i >= 0; i--) {
             while (!st.empty() && arr[st.top()] > arr[i]) st.pop();
-            if (!st.empty()) neRight[i] = st.top();
+            if (!st.empty()) nseRight[i] = st.top();
             st.push(i);
         }
 
         long long minSum = 0;
         for (int i = 0; i < arr.size(); i++) {
-            long long l = neLeft[i] == -1 ? 0 : neLeft[i]+1;
-            long long r = neRight[i] == -1 ? arr.size()-1 : neRight[i]-1;
+            long long l = nseLeft[i] == -1 ? 0 : nseLeft[i]+1;
+            long long r = nseRight[i] == -1 ? arr.size()-1 : nseRight[i]-1;
 
             long long n = r-l+1;
             long long modifiedI = i-l+1;
 
             minSum += arr[i] * (modifiedI * (n - modifiedI + 1));
-            neRight[i] = -1;
-            neLeft[i] = -1;
         }
 
         while (!st.empty()) st.pop();
 
+
+        vector<int> ngeLeft(arr.size(), -1);
+        vector<int> ngeRight(arr.size(), -1);
+
         for (int i = 0; i < arr.size(); i++) {
             while (!st.empty() && arr[st.top()] <= arr[i]) st.pop();
-            if (!st.empty()) neLeft[i] = st.top();
+            if (!st.empty()) ngeLeft[i] = st.top();
             st.push(i);
         }
 
@@ -48,14 +50,14 @@ public:
 
         for (int i = arr.size() - 1; i >= 0; i--) {
             while (!st.empty() && arr[st.top()] < arr[i]) st.pop();
-            if (!st.empty()) neRight[i] = st.top();
+            if (!st.empty()) ngeRight[i] = st.top();
             st.push(i);
         }
 
         long long maxSum = 0;
         for (int i = 0; i < arr.size(); i++) {
-            long long l = neLeft[i] == -1 ? 0 : neLeft[i]+1;
-            long long r = neRight[i] == -1 ? arr.size()-1 : neRight[i]-1;
+            long long l = ngeLeft[i] == -1 ? 0 : ngeLeft[i]+1;
+            long long r = ngeRight[i] == -1 ? arr.size()-1 : ngeRight[i]-1;
 
             long long n = r-l+1;
             long long modifiedI = i-l+1;
