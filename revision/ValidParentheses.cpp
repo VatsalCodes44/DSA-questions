4c5abefede757085e16e4428e4a321a813159def
Vatsal Mahajan <mahajanvatsal44@gmail.com>
2026-08-22 13:48:03 +0530

    ValidParentheses

---
+++ revision/ValidParentheses.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for (auto i: s) {
+            if (i == '(' || i == '{' || i == '[') st.push(i);
+            else if (st.empty()) return false;
+            else if ((i == ')' && st.top() == '(') || (i == ']' && st.top() == '[') || (i == '}' && st.top() == '{')) st.pop();
+            else return false;
+        }
+        if (!st.empty())return false;
+        return true;
+    }
+};
