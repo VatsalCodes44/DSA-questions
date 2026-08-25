99647dffa33e653f11a1900727416e817c38bef7
Vatsal Mahajan <mahajanvatsal44@gmail.com>
2026-08-25 16:28:47 +0530

    generate all the subseq approach 2

--- revision/generateAllTheSubseq.cpp
+++ revision/generateAllTheSubseq.cpp
@@ -24,9 +24,21 @@ void subSeq(vector<int> &arr, int i, vector<int>& ans) {
     subSeq(arr, i+1, ans);
 }
 
+void subSeq2(vector<int> &arr, int i, vector<int>& ans) {
+    printVec(ans);
+    
+    if (i == arr.size()) return;
+
+    for (int j = i; j < arr.size(); j++) {
+        ans.push_back(arr[j]);
+        subSeq2(arr, j+1, ans);
+        ans.pop_back();
+    }
+}
+
 int main () {
     vector<int> arr = {1,2,3};
     vector<int> ans;
 
-    subSeq(arr, 0, ans);
+    subSeq2(arr, 0, ans);
 }
