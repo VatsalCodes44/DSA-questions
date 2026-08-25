a79746ec743804d84768b7435314bee0f312171c
Vatsal Mahajan <mahajanvatsal44@gmail.com>
2026-08-25 22:25:24 +0530

    modified

--- revision/binary-strings.cpp
+++ revision/binary-strings.cpp
@@ -12,7 +12,7 @@ vector<string> binaryStrings(string& ans, int i, int n, vector<string>& ansArr) 
     binaryStrings(ans, i+1, n, ansArr);
     ans.pop_back();
 
-    if (ans.size() == 0 || (ans.size() > 0 && ans.back() != '1')) {
+    if (ans.size() == 0 ||  ans.back() != '1') {
         ans.push_back('1');
         binaryStrings(ans, i+1, n, ansArr);
         ans.pop_back();
