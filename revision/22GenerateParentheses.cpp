869a1d8049f2c398d03a7820f7a0eaea5b0f4b73
Vatsal Mahajan <mahajanvatsal44@gmail.com>
2026-08-25 22:40:40 +0530

    22GenerateParentheses

---
+++ revision/22GenerateParentheses.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    void f(int n, int open, int close, string& ans, vector<string>& ansArr) {
+        if (close == n) {
+            ansArr.push_back(ans);
+            return;
+        }
+
+        if (open < n) {
+            ans.push_back('(');
+            f(n, open+1, close, ans, ansArr);
+            ans.pop_back();
+        }
+
+        if (close < open) {
+            ans.push_back(')');
+            f(n, open, close+1, ans, ansArr);
+            ans.pop_back();
+        }
+    }
+     vector<string> generateParenthesis(int n) {
+        string ans;
+        vector<string> ansArr;
+        f(n, 0, 0, ans, ansArr);
+        return ansArr;
+    }
+};
+
+int main () {
+    string s = "aaa";
+    s[1] = 'b';
+    cout << s;
+}
