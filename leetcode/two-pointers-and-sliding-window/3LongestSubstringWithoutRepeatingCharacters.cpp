bd6f50e245bd848d0e2f1f7caea1cb1f3615599f
Vatsal Mahajan <mahajanvatsal44@gmail.com>
2026-09-01 04:29:10 +0530

    optimal lengthOfLongestSubstring

--- leetcode/two-pointers-and-sliding-window/3LongestSubstringWithoutRepeatingCharacters.cpp
+++ leetcode/two-pointers-and-sliding-window/3LongestSubstringWithoutRepeatingCharacters.cpp
@@ -6,19 +6,23 @@ class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
         int l = 0, r = 0, maxLen = 0;
-        int hash[256] = {0};
+        vector<int> hash(256, -1);
 
         while (r < s.size()) {
-            while (hash[s[r]] > 0) {
-                hash[s[l]]--;
-                l++;
+            if (hash[s[r]] >= l) {
+                l = hash[s[r]]+1;
             }
 
             maxLen = max(maxLen, r-l+1);
-            hash[s[r]]++;
+            hash[s[r]] = r;
             r++;
         }
 
         return maxLen;
     }
 };
+
+int main () {
+    Solution s;
+    cout << s.lengthOfLongestSubstring("S") << endl;
+}
