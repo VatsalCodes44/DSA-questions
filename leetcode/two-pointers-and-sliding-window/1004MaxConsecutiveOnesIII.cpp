3c2ceeeb3724463ecbcb79948e0ab066a9cf90f0
Vatsal Mahajan <mahajanvatsal44@gmail.com>
2026-09-01 05:30:23 +0530

    leetcode/two-pointers-and-sliding-window/1004MaxConsecutiveOnesIII.cpp

---
+++ leetcode/two-pointers-and-sliding-window/1004MaxConsecutiveOnesIII.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+        int l = 0, r = 0;
+        int maxLen = 0;
+        int count = 0;
+        
+        while ( r < nums.size()) {
+            if (nums[r] == 0) count++;
+            if (count > k) {
+                if (nums[l] == 0) count--;
+                l++;
+            }
+            if (count <= k) {
+                maxLen = max(maxLen, r-l+1);
+            }
+            r++;
+        }
+        return maxLen;
+    }
+};
