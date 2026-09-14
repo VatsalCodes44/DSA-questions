10d41c70bc740c50c4aff1cc9b17ffab77e0592b
Vatsal Mahajan <mahajanvatsal44@gmail.com>
2026-09-14 17:04:10 +0530

    jump game 2

--- leetcode/greedy/45-Jump-Game-II.cpp
+++ leetcode/greedy/45-Jump-Game-II.cpp
@@ -5,24 +5,18 @@ using namespace std;
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        if (nums.size() == 1) return 0;
-        int l = 0, r = 0;
-        int jump = 0;
-        while (l <= r) {
-            if (r >= nums.size()-1) break;
-            int minOutOfRange = 0;
-            int maxOutOfRange = 0;
-            for (int j = l; j <= r; j++) {
-                if (j+nums[j] <= r) continue;
-                else {
-                    maxOutOfRange = max(maxOutOfRange, j+nums[j]);
-                    minOutOfRange = r+1;
-                }
+        int jumps = 0, l = 0, r = 0;
+
+        while (r < nums.size()-1) {
+            int farthest = 0;
+            for (int i = l; i <= r; i++) {
+                farthest = max(farthest, i+nums[i]);
             }
-            r = maxOutOfRange;
-            l = minOutOfRange;
-            jump++;
+            l = r+1;
+            r = farthest;
+            jumps++;
         }
-        return jump;
+
+        return jumps;
     }
 };
