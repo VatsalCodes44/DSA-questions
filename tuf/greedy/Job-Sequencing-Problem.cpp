ca4ee515cca0bfdf75ca18219b937ab42dcf6498
Vatsal Mahajan <mahajanvatsal44@gmail.com>
2026-09-16 18:30:00 +0530

    job seq

--- tuf/greedy/Job-Sequencing-Problem.cpp
+++ tuf/greedy/Job-Sequencing-Problem.cpp
@@ -5,33 +5,47 @@ using namespace std;
 class Solution {
 	public:
 	static bool comparator(pair<int, int> a, pair<int, int> b) {
-		if (a.first == b.first) return a.second > b.second;
-		return a.first < b.first;
+		if (a.second == b.second) return a.first < b.first;
+		return a.second > b.second;
 	}
 	vector<int> jobSequencing(vector<int> &deadline, vector<int> &profit) {
 		// code here
 		vector<pair<int, int>> jobs;
 		jobs.reserve(deadline.size());
 		
+		int maxDeadline = -1;
+		
 		for (int i = 0; i < deadline.size(); i++) {
+		    maxDeadline = max(maxDeadline, deadline[i]);
 		    jobs.push_back({deadline[i], profit[i]});
 		}
 		
 		sort(jobs.begin(), jobs.end(), comparator);
 		
-		int totalProfit = 0;
-		int totalJobs = 0;
-		int t = 0;
-
+		vector<int> endDays(maxDeadline+1, -1);
+		
+		int p = 0;
+		int c = 0;
 		for (int i = 0; i < jobs.size(); i++) {
-		    if (jobs[i].first > t) {
-		        t++;
-		        totalProfit += jobs[i].second;
-		        totalJobs++;
+		    int d = jobs[i].first;
+		    while (d >= 1 && endDays[d] != -1) d--;
+		    
+		    if (d >= 1) {
+		        endDays[d] = 1;
+		        p += jobs[i].second;
+		        c++;
 		    }
 		}
 		
-		vector <int> ans = {totalJobs, totalProfit};
+		vector<int> ans; ans.push_back(c); ans.push_back(p);
 		return ans;
+		
 	}
 };
+
+int main () {
+    vector<int> deadline = {4, 1, 1, 1};
+    vector<int> profit = {20, 10, 40, 30};
+    Solution s;
+    s.jobSequencing(deadline, profit);
+}
