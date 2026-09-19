d4dd862dfa888888c57e3618517b7a41ea1369c0
Vatsal Mahajan <mahajanvatsal44@gmail.com>
2026-09-19 16:47:03 +0530

    lemonade chane

--- revision/Lemonade-Change.cpp
+++ revision/Lemonade-Change.cpp
@@ -5,28 +5,30 @@ using namespace std;
 class Solution {
 public:
     bool lemonadeChange(vector<int>& bills) {
-        int b5 = 0; 
-        int b10 = 0;
-        int b15 = 0;
+        int b5 = 0, b10 = 0, b20 = 0;
 
-        for (int i = 0; i < bills.size(); i++) {
-            if (bills[i] == 5) {
-                b5++;
-            }
-            else if (bills[i] == 10) {
-                if (b5 >= 1 ) {
-                    b5--;
-                    b10++;
-                }
-                else return false;
-            }
-            else {
+        for (int j = 0; j < bills.size(); j++) {
+            int i = bills[j];
+            if (i == 20) {
+                b20++;
                 if (b10 >= 1 && b5 >= 1) {
                     b10--;
                     b5--;
                 }
-                else if (b5 >= 3) b5-=3;
+                else if (b5 >= 3) {
+                    b5 -= 3;
+                }
                 else return false;
+            }
+            else if (i == 10) {
+                b10++;
+                if (b5 >= 1) {
+                    b5--;
+                }
+                else return false;
+            }
+            else {
+                b5++;
             }
         }
         return true;
