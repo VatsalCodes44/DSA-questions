60d4e8d4d801f9fef9065383b3f2220b5c915a61
Vatsal Mahajan <mahajanvatsal44@gmail.com>
2026-09-23 14:12:58 +0530

    jump game

--- revision/45-Jump-Game-II.cpp
+++ revision/45-Jump-Game-II.cpp
@@ -2,20 +2,17 @@
 
 using namespace std;
 
-class Solution {
-public:
-    int jump(vector<int>& nums) {
-        int l = 0; int r = 0;
-        int count = 0;
-        while (r < nums.size()-1) {
-            int newMax = 0;
-            while (l <= r) {
-                newMax = max(newMax, l+nums[l]);
-                l++;
-            }
-            r = newMax;
-            count++;
+int jump(vector<int>& nums) {
+    int count = 0;
+    int l = 0; int r = 0;
+    while (r < nums.size()-1) {
+        int newMax = 0;
+        while (l <= r) {
+            newMax = max(newMax, l+nums[l]);
+            l++;
         }
-        return count;
+        r = newMax;
+        count++;
     }
-};
+    return count;
+}
